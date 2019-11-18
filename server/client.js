fa26fb11dc2a10c39f60d85b211e375fdcfdc3de
Vivek Ramesh <zup@Viveks-iMac.local>
2019-11-18 18:35:19 +0530

    redis client on default db on no pas

--- server/client.js
+++ server/client.js
@@ -2,9 +2,7 @@ const redis = require('redis')
 
 const client = redis.createClient({
   host: 'localhost',
-  port: 6379,
-  password: '1CO%3Hm%EkdYXWrYsDs7H%YMuKp4gqcJF0sI1iHybu7i9U6z',
-  db: 1
+  port: 6379
 })
 
 module.exports = client
